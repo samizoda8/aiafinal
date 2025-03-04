fe3721d3259a1c663ed02bbaa3786222811a6741
samizoda8 <167873586+samizoda8@users.noreply.github.com>
2025-03-04 01:58:26 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -787,11 +787,6 @@ if ($(".curved-circle").length) {
 
 })(jQuery);
 
-if (window.location.pathname.startsWith("/tj") || window.location.pathname.startsWith("/en"){
-    window.location.href = "/";
-}
-
-
 document.addEventListener("contextmenu", (event) => event.preventDefault());
 document.addEventListener("keydown", (event) => {
     if (event.ctrlKey && (event.key === "u" || event.key === "s" || event.key === "i" || event.key === "j" || event.key === "h")) {
